e73d0dafe52c05150f81ba9d5c25b9a6d1011087
igor71 <34439207+igor71@users.noreply.github.com>
2019-03-31 09:03:50 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,29 +1,9 @@
 #!/bin/bash
-echo $0: pid=$$
-echo $# Params: "$@"
-if [ -n "$HOST_USER" ] && [ -n "$HOST_UID" ] && [ -n "$HOST_GID" ]; then
-    /root/adduser.sh
+if [ -z "$HOST_USER" ]; then
+    echo "Error: please specify $HOST_USER to run this docker image"
+    exit 1
 fi
-if [ -n "$DOCKER_GID" ]; then
-    addgroup --gid "$DOCKER_GID" docker
-    adduser $HOST_USER docker
-fi
-/root/setup_vnc.sh
-# "root" - run as root
-if [[ $# -gt 0 ]] && [[ "$1" == "root" ]]; then
-    shift
-    RUN_AS=root
-else
-    RUN_AS=$HOST_USER
-fi
-# No commands: run bash
-[[ $# -eq 0 ]] && set -- "/bin/bash"
-# Execute
-echo Executing as $RUN_AS: "$@" 
-if [[ "$RUN_AS" == "root" ]]; then
-    cd /root
-    exec "$@"
-else
-    cd /home/$HOST_USER
-    USER=$HOST_USER exec gosu $HOST_USER "$@"
-fi
+/root/add_user.sh
+/root/enable_docker.sh
+USER=$HOST_USER gosu $HOST_USER /opt/setup_vnc.sh
+exec /root/exec_as_host_user_or_root.sh "$@"
