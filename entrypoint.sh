1c6d1cbad9c20abbf079677ec9898ac5cdc9b063
igor71 <34439207+igor71@users.noreply.github.com>
2019-04-07 13:39:21 +0300

    Delete entrypoint.sh

--- entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-if [ -z "$HOST_USER" ]; then
-    echo "Error: please specify $HOST_USER to run this docker image"
-    exit 1
-fi
-/root/add_user.sh
-/root/enable_docker.sh
-USER=$HOST_USER gosu $HOST_USER /opt/setup_vnc.sh
-exec /root/exec_as_host_user_or_root.sh "$@"
